652f8385e5af2e60b4eb0d3ada860aa192bdd5fb
Malcolm Ramsay <malramsay64@gmail.com>
2018-01-13 16:37:31 +1100

    Include testing for running from command line
    Ensuring the functionality from the command line is working as expected.

---
+++ test/experirun_test.py
@@ -0,0 +1,18 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+# vim:fenc=utf-8
+#
+# Copyright © 2018 Malcolm Ramsay <malramsay64@gmail.com>
+#
+# Distributed under terms of the MIT license.
+
+"""Test the command line interface to experirun."""
+
+import subprocess
+
+
+def test_command():
+    proc = subprocess.run(['experirun'], stdout=subprocess.PIPE, cwd='test/data')
+    with open('test/data/result.txt', 'rb') as source:
+        expected = source.read()
+    assert proc.stdout == expected
