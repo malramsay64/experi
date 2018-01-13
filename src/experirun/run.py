416e956267c05722733407d72be6aed417b611a4
Malcolm Ramsay <malramsay64@gmail.com>
2018-01-13 15:58:06 +1100

    Cleanup and removal of code
    There were some remnants from when I was testing some ideas which have
    now been removed. Also some modifications for consistency and clarity in
    the code.

--- src/experirun/run.py
+++ src/experirun/run.py
@@ -8,21 +8,13 @@
 
 """Run an experiment varying a number of variables."""
 
-from itertools import product
-from string import Formatter
-from typing import Any, Dict, List
+import subprocess
 from collections import ChainMap
-from subprocess import call
+from itertools import product
 from os import PathLike
+from typing import Any, Dict, List
 
 import yaml
-
-
-def collect_format_vars(string: str) -> List[str]:
-    """Extract the variables from a format string to a list."""
-    # Parse retuns a tuple of 4 values being (literal_text, field_name, format_spec, conversion)
-    # This function is only concerned with the field_name so the rest is ignored.
-    return [field_name for _, field_name, *_ in Formatter().parse(string) if field_name is not None]
 
 
 def combine_dictionaries(dicts: List[Dict[str, Any]]) -> Dict[str, Any]:
@@ -56,9 +48,6 @@ def process_command(structure: Dict[str, Any]) -> List[str]:
     # Create variable matrix
     matrix = variable_matrix(structure.get('variables'))
 
-    # Check variables in command exist
-    command_vars = collect_format_vars(structure.get('command'))
-
     # substitute variables into command
     return [structure.get('command').format(**kwargs) for kwargs in matrix]
 
@@ -78,4 +67,4 @@ def main() -> None:
 
     # Run commands
     for command in commands:
-        call(command.split())
+        subprocess.run(command.split())
