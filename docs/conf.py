fca62bd763d0d3ef0bc19b7968732d5536502fb4
Malcolm Ramsay <malramsay64@gmail.com>
2018-07-18 10:14:49 +1000

    DOC Import versioning from experi

--- docs/conf.py
+++ docs/conf.py
@@ -12,9 +12,12 @@
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #
-# import os
-# import sys
-# sys.path.insert(0, os.path.abspath('.'))
+import os
+import sys
+
+sys.path.append(os.path.abspath("../src"))  # isort:skip
+
+from experi.version import __version__  # isort:skip
 
 
 # -- Project information -----------------------------------------------------
@@ -24,9 +27,9 @@ copyright = "2018, Malcolm Ramsay"
 author = "Malcolm Ramsay"
 
 # The short X.Y version
-version = ""
+version = __version__
 # The full version, including alpha/beta/rc tags
-release = "0.2.6"
+release = __version__
 
 
 # -- General configuration ---------------------------------------------------
